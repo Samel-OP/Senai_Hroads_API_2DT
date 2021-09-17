68ae21543c4f932321928e7541fb5d509b25826f
Vinicius <vini.evangelista@live.com>
2021-09-17 10:52:43 -0300

    Interfaces feitas, falta só classe

---
+++ HROADS-SENAI/senai.hroads.webApi/senai.hroads.webApi/Interfaces/ITipoHabilidadeRepository.cs
@@ -0,0 +1,51 @@
+﻿using senai.hroads.webApi_.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi_.Interfaces
+{
+    interface ITipoHabilidadeRepository
+    {
+        //Cadastrar
+        //Listar
+        //Atualizar
+        //ListarPorId
+        //Deletar
+
+        /// <summary>
+        /// Cadastra um novo tipo de habilidade
+        /// </summary>
+        /// <param name="novoTipoUsuario">Objeto contendo as informações do novo tipo de habilidade</param>
+        void Cadastrar(TiposHabilidade novoTipoHabilidade);
+
+        /// <summary>
+        /// Lista todos os tipos de habilidade
+        /// </summary>
+        /// <returns> uma lista de tipos de habilidade</returns>
+        List<TiposHabilidade> Listar();
+
+
+        /// <summary>
+        /// atualiza um tipo de habilidae pelo id
+        /// </summary>
+        /// <param name="id">id do tipo habilidade que será atualizado</param>
+        /// <param name="tipoHabilidadeAtualizado">Objeto contendo o tipo habilidade atuaizado</param>
+        void Atualizar(int id, TiposUsuario tipoHabilidadeAtualizado);
+
+        /// <summary>
+        /// busca um tipo habilidade pelo id
+        /// </summary>
+        /// <param name="id"> id do tipo usuari que será buscado</param>
+        /// <returns>o tipo usuariio buscado</returns>
+        TiposUsuario BuscarPorId(int id);
+
+
+        /// <summary>
+        /// deleta um tipo de usuario
+        /// </summary>
+        /// <param name="id">id do tipo de usuario passado</param>
+        void Deletar(int id);
+    }
+}
---
+++ HROADS-SENAI/senai.hroads.webApi/senai.hroads.webApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,51 @@
+﻿using senai.hroads.webApi_.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        //Cadastrar
+        //Listar
+        //Atualizar
+        //ListarPorId
+        //Deletar
+
+        /// <summary>
+        /// Cadastra um novo tipo de usuario 
+        /// </summary>
+        /// <param name="novoUsuario">Objeto contendo as informações do</param>
+        void Cadastrar(Usuario novoUsuario);
+
+        /// <summary>
+        /// Lista todos os tipos de usuario
+        /// </summary>
+        /// <returns> uma lista de usuario</returns>
+        List<Usuario> Listar();
+
+
+        /// <summary>
+        /// atualiza um tipo de usuario pelo id
+        /// </summary>
+        /// <param name="id">id do tipo usuario que será atualizado</param>
+        /// <param name="UsuarioAtualizado">Objeto contendo o tipo usuario atuaizado</param>
+        void Atualizar(int id, Usuario UsuarioAtualizado);
+
+        /// <summary>
+        /// busca um tipo usuario pelo id
+        /// </summary>
+        /// <param name="id"> id do tipo usuario que será buscado</param>
+        /// <returns>o tipo usuariio buscado</returns>
+        Usuario BuscarPorId(int id);
+
+
+        /// <summary>
+        /// deleta um tipo de usuario
+        /// </summary>
+        /// <param name="id">id do tipo de usuario passado</param>
+        void Deletar(int id);
+    }
+}

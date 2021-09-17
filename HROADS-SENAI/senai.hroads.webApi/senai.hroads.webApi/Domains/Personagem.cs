98991603c58e4d4fba5285db01d386903b84e00e
DEV <dev@info.local>
2021-09-17 14:41:48 -0300

    interfaces e domains feitas

--- HROADS-SENAI/senai.hroads.webApi/senai.hroads.webApi/Domains/Personagem.cs
+++ HROADS-SENAI/senai.hroads.webApi/senai.hroads.webApi/Domains/Personagem.cs
@@ -31,5 +31,28 @@ namespace senai.hroads.webApi_.Domains
         public int idPersonagem { get; set; }
 
         public int idClasse { get; set; }
+
+        [Column(TypeName = "VARCHAR(70)" )] //tipo de dado da coluna
+        [Required(ErrorMessage ="O campo é obrigatório !")] //not null
+        public string NomePersongaem { get; set; }
+           
+        [Column(TypeName = "SMALLINT")]
+        [Required(ErrorMessage = "O campo é obrigatório !")]
+        public int capacidadeDeVidaMax { get; set; }
+
+        [Column(TypeName = "TINYINT")]
+        [Required(ErrorMessage = "O campo é obrigatório !")]
+        public int capacidadeDeManaMax { get; set; }
+
+        [Column(TypeName = "DATE")]
+        [Required(ErrorMessage = " O campo é obrigatório !")]
+        public DateTime dataUtilizacao { get; set; }
+
+        [Column(TypeName = "DATE")]
+        [Required(ErrorMessage = "O campo é obrigatório !")]
+        public DateTime dataCriacao { get; set; }
+
+        [ForeignKey("idClasse")]
+        public Classe classe { get; set; }
     }
 }
